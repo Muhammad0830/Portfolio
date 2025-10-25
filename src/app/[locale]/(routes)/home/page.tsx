cc9ff2547c6b9e85885bc46dfd91b358cb6f2e61
Muhammad0830 <abduqayumovm1@gmail.com>
2025-10-25 17:15:23 +0500

    i18n config is done

---
+++ src/app/[locale]/(routes)/home/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { Link, usePathname } from "@/i18n/navigation";
+import { cn } from "@/lib/cn";
+import { useTranslations } from "next-intl";
+import { usePathname as nextUsePathname } from "next/navigation";
+
+const Page = () => {
+  const t = useTranslations();
+  const pathname = usePathname();
+  const nextPathname = nextUsePathname();
+
+  return (
+    <div>
+      <span>Next app</span>
+
+      <div className="flex flex-col gap-1 items-start">
+        <Link
+          className={cn(
+            "border px-3 py-1.5 rounded-sm",
+            nextPathname.includes("/uz") && "bg-white/20"
+          )}
+          href={pathname}
+          locale="uz"
+        >
+          O&apos;zbekcha
+        </Link>
+        <Link
+          className={cn(
+            "border px-3 py-1.5 rounded-sm",
+            nextPathname.includes("/en") && "bg-white/20"
+          )}
+          href={pathname}
+          locale="en"
+        >
+          English
+        </Link>
+      </div>
+
+      <span>{t("hello")}</span>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { routing } from "./i18n/routing";
+import createMiddleware from "next-intl/middleware";
+
+const nextIntlMiddleware = createMiddleware(routing);
+
+const defaultLocale = routing.defaultLocale;
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname === "/") {
+    return NextResponse.redirect(
+      new URL(`/${defaultLocale}/home`, request.url)
+    );
+  }
+
+  return nextIntlMiddleware(request);
+}
+
+export const config = {
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+};
